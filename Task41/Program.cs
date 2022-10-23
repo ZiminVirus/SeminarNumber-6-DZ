d7e7d65233e1f031b9b1ecda40f748e8a9691ea1
Alexandr <ziminvirus@yandex.ru>
2022-10-23 19:49:47 +0300

    Task43

---
+++ Task41/Program.cs
@@ -0,0 +1,25 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// -1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите сколько чисел Вы хотите ввести ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int InputNumber(int num)
+{
+    int ferstnumber = default;
+    for (int i = 0; i < num; i++)
+    {
+        Console.WriteLine("Введите число ");
+        int secondnumber = Convert.ToInt32(Console.ReadLine());
+        if (secondnumber > 0) ferstnumber++;
+    }
+    return ferstnumber;
+}
+
+int inputNumber = InputNumber(number);
+Console.WriteLine($"-> {inputNumber}");
---
+++ Task43/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 43: Напишите программу, 
+// которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите значение b1 ");
+int b1number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение k1 ");
+int k1number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение b2 ");
+int b2number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение k2 ");
+int k2number = Convert.ToInt32(Console.ReadLine());
+
+
+double CoordinateX(int b1num, int k1num, int b2num, int k2num)
+{
+    double x = Convert.ToDouble(b2num - b1num) / (k1num - k2num);
+    return (x);
+}
+
+
+double CoordinateY(int b1numb, int k1numb, int b2numb, int k2numb)
+{
+    double coordinateX = CoordinateX(b1number, k1number, b2number, k2number);
+    double y = Convert.ToDouble(k1numb * (coordinateX) + b1numb);
+    return (y);
+}
+
+
+double coordinateX = CoordinateX(b1number, k1number, b2number, k2number);
+double coordinateY = CoordinateY(b1number, k1number, b2number, k2number);
+Console.WriteLine($"b1 = {b1number}, k1 = {k1number}, b2 = {b2number}, k2 = {k2number} -> ({coordinateX}; {coordinateY})");
